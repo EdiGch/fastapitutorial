b62b03457e6ded6f7f2539de3e5fba858afbc2af
Grzegorz Charkiewicz <gcharkiewicz@enzode.com>
2021-11-22 13:32:31 +0100

    FastAPI Route !

--- backend/main.py
+++ backend/main.py
@@ -1,21 +1,20 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from core.config import settings
-from apis.general_pages.route_homepage import general_pages_router
+from apis.base import api_router
 from db.session import engine
 from db.base import Base
 
 
 def include_router(app):
-    app.include_router(general_pages_router)
+    app.include_router(api_router)
 
 
-def configure_static(app):  # new
+def configure_static(app):
     app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 def create_tables():
-    print("create_tables")
     Base.metadata.create_all(bind=engine)
 
 def start_application():
